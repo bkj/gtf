44e7f986dee01485530507fdb15cfa4fb3399664
Ben Johnson <bkj.322@gmail.com>
2018-08-27 14:44:07 -0400

    testing

--- gtf/main.cpp
+++ gtf/main.cpp
@@ -1,3 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -7,71 +14,93 @@
 #include <map>
 #include <set>
 #include <sstream>
-#include "graph.h"
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <limits.h>
 #include "graph.h"
-#include <time.h>
 
 using namespace std;
 
-void graph_tv(double *Y,// value of nodes
-        int n, //number of nodes
-        int m, // number of edges
-        int* edges1,
-        int* edges2,
-//an array of edges of size m. There is an edge edges1[i] -> edges2[i]
+void graph_tv(double *node_values,  // value of nodes
+        int num_nodes,    // number of nodes
+        int num_edges,    // number of edges
+        int* edge_src,
+        int* edge_trg,
         double lambda,
-        float erreur)
-{
-        int i;
-    double dval,dr,drd;
+        float error) {
+    
+    double dval, dr, drd;
 
-    dr=lambda;
+    dr = lambda;
 
     Graph::node_id * nodes, no;
 
-    if (n <=2 || m <= 2)
-        { fprintf(stderr,"error: bad size\n"); exit(0); }
+    if (num_nodes <= 2 || num_edges <= 2) {
+        fprintf(stderr,"error: bad size\n");
+        exit(0);
+    }
 
-    nodes = (Graph::node_id *) malloc(n*sizeof(Graph::node_id));
-    Graph *BKG = new Graph();
-    for (i=0;i<n;i++) {
-        no=nodes[i]=BKG->add_node();
-        BKG->set_tweights(no,Y[i],0.);
+    nodes = (Graph::node_id *) malloc(num_nodes * sizeof(Graph::node_id));
+    Graph *G = new Graph();
+    
+    // Initialize nodes
+    for (int i = 0; i < num_nodes; i++) {
+        no = nodes[i] = G->add_node();
+        G->set_tweights(no, node_values[i], 0.);
     }
-    for (i=0;i<m;i++) {
-        BKG->add_edge(nodes[edges1[i]],nodes[edges2[i]],dr,dr);
+    
+    // Initialize edges
+    for (int i = 0; i < num_edges; i++) {
+        G->add_edge(nodes[edge_src[i]], nodes[edge_trg[i]], dr, dr);
     }
-    BKG->dyadicparametricTV(erreur);
-    for (i=0;i<n;i++) Y[i]=BKG->what_value(nodes[i]);
-         delete BKG; free(nodes);
+    
+    // Run dyadicparametricTV
+    G->dyadicparametricTV(error);
+    
+    // Export values
+    for (int i = 0; i < num_nodes; i++) {
+        node_values[i] = G->what_value(nodes[i]);
+    }
+    
+    delete G;
+    free(nodes);
 }
 
 int main(){
 
-    double lambda1 = 3;
-
-    int n = 4;
-    int e1[] = {0, 0, 1, 1, 2, 3};
-    int e2[] = {1, 2, 0, 3, 1, 2};
-    int m = 6;
-
-    double *Y = new double[n];
-    Y[0] = 16;
-    Y[1] = 13;
-    Y[2] = -20;
-    Y[3] = -4;
-
-	  graph_tv(Y, n, m, e1, e2, lambda1, 0.0);
-
-    cout << endl << "gtf results are (not tr_cap)" << endl;
-    for(int i = 0; i < n; i++){
-        cout << Y[i] << endl;
+    double lambda = 3;
+    int edge_src[] = {0, 0, 1, 1, 2, 3};
+    int edge_trg[] = {1, 2, 0, 3, 1, 2};
+    
+    // Compute number of edges
+    int num_edges = sizeof(edge_src) / sizeof(edge_src[0]);
+    
+    // Compute number of nodes
+    int num_nodes = 0;
+    for (int i = 0; i < num_edges; i++) {
+        if (edge_src[i] > num_nodes) {
+            num_nodes = edge_src[i];
+        }
+        if (edge_trg[i] > num_nodes) {
+            num_nodes = edge_trg[i];
+        }        
     }
+    num_nodes++;
+    
+    cout << "num_nodes: " << num_nodes << endl;
+    cout << "num_edges: " << num_edges << endl;
+
+    // Initial node values
+    double *node_values = new double[num_nodes];
+    node_values[0] = 16;
+    node_values[1] = 13;
+    node_values[2] = -20;
+    node_values[3] = -4;
+
+    graph_tv(node_values, num_nodes, num_edges, edge_src, edge_trg, lambda, 0.0);
+
+    cout << endl << "Results:" << endl;
+    for(int i = 0; i < num_nodes; i++){
+        cout << node_values[i] << endl;
+    }
+    
     return 0;
 }
--- min.cpp
+++ min.cpp
@@ -1,9 +1,10 @@
 // C++ program for finding minimum cut using Ford-Fulkerson
 #include <stdio.h>
-#include <iostream>
 #include <limits.h>
 #include <string.h>
+#include <iostream>
 #include <queue>
+
 using namespace std;
 
 // Number of vertices in given graph
@@ -11,8 +12,7 @@ using namespace std;
 typedef double captype;
 /* Returns true if there is a path from source 's' to sink 't' in
   residual graph. Also fills parent[] to store the path */
-int bfs(double rGraph[V][V], int s, int t, int parent[])
-{
+int bfs(double rGraph[V][V], int s, int t, int parent[]) {
     // Create a visited array and mark all vertices as not visited
     bool visited[V];
     memset(visited, 0, sizeof(visited));
@@ -25,15 +25,12 @@ int bfs(double rGraph[V][V], int s, int t, int parent[])
     parent[s] = -1;
 
     // Standard BFS Loop
-    while (!q.empty())
-    {
+    while (!q.empty()) {
         int u = q.front();
         q.pop();
 
-        for (int v=0; v<V; v++)
-        {
-            if (visited[v]==false && rGraph[u][v] > 0)
-            {
+        for (int v = 0; v < V; v++) {
+            if (visited[v] == false && rGraph[u][v] > 0) {
                 q.push(v);
                 parent[v] = u;
                 visited[v] = true;
@@ -49,8 +46,7 @@ int bfs(double rGraph[V][V], int s, int t, int parent[])
 // A DFS based function to find all reachable vertices from s.  The function
 // marks visited[i] as true if i is reachable from s.  The initial values in
 // visited[] must be false. We can also use BFS to find reachable vertices
-void dfs(double rGraph[V][V], int s, bool visited[])
-{
+void dfs(double rGraph[V][V], int s, bool visited[]) {
     visited[s] = true;
     for (int i = 0; i < V; i++)
        if (rGraph[s][i] && !visited[i])
@@ -58,14 +54,13 @@ void dfs(double rGraph[V][V], int s, bool visited[])
 }
 
 // Prints the minimum s-t cut
-void minCut(double graph[V][V], int s, int t)
-{
+void minCut(double graph[V][V], int s, int t) {
     int u, v;
 
     // Create a residual graph and fill the residual graph with
     // given capacities in the original graph as residual capacities
     // in residual graph
-    double rGraph[V][V]; // rGraph[i][j] indicates residual capacity of edge i-j
+    double rGraph[V][V];  // rGraph[i][j] indicates residual capacity of edge i-j
     for (u = 0; u < V; u++)
         for (v = 0; v < V; v++)
              rGraph[u][v] = graph[u][v];
@@ -73,31 +68,29 @@ void minCut(double graph[V][V], int s, int t)
     int parent[V];  // This array is filled by BFS and to store path
 
     // Augment the flow while there is a path from source to sink
-    while (bfs(rGraph, s, t, parent))
-    {
+    while (bfs(rGraph, s, t, parent)) {
         // Find minimum residual capacity of the edhes along the
         // path filled by BFS. Or we can say find the maximum flow
         // through the path found.
         double path_flow = INT_MAX;
-        for (v=t; v!=s; v=parent[v])
-        {
+        for (v = t; v != s; v = parent[v]) {
             u = parent[v];
             path_flow = min(path_flow, rGraph[u][v]);
         }
 
         // update residual capacities of the edges and reverse edges
         // along the path
-        for (v=t; v != s; v=parent[v])
-        {
+        for (v=t; v != s; v=parent[v]) {
             u = parent[v];
             rGraph[u][v] -= path_flow;
             rGraph[v][u] += path_flow;
         }
 
         printf("residual graph printout\n");
-        for (u = 0; u < V; u++){
-            for (v = 0; v < V; v++)
+        for (u = 0; u < V; u++) {
+            for (v = 0; v < V; v++) {
                  printf("%5.2f ", graph[u][v]);
+            }
             printf("\n");
         }
         printf("\n");
@@ -110,46 +103,46 @@ void minCut(double graph[V][V], int s, int t)
 
     // Print all edges that are from a reachable vertex to
     // non-reachable vertex in the original graph
-    for (int i = 0; i < V; i++)
-      for (int j = 0; j < V; j++){
-         if (visited[i] && !visited[j] && graph[i][j])
-              cout << i << " - " << j << endl;
-      }
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (visited[i] && !visited[j] && graph[i][j]) {
+                cout << i << " - " << j << endl;
+            }
+        }
+    }
     return;
 }
 
 // Driver program to test above functions
-int main()
-{
-
+int main() {
     double lambda1 = 3;
 
-    int n = V-2;
+    int n    = V - 2;
     int e1[] = {0, 0, 1, 1, 2, 3};
     int e2[] = {1, 2, 0, 3, 1, 2};
-    int m = 6;
+    int m    = 6;
 
     double tr_cap[V-2];
     double label[V-2];
 
     double graph[V][V] = {0};
-    for(int i = 0; i < m; i++){
+    for (int i = 0; i < m; i++) {
       graph[e1[i]+2][e2[i]+2] = lambda1;
     }
 
     // normalization as did in parametric maxflow
-    graph[0][2] = 16-1.25;
-    graph[0][3] = 13-1.25;
-    graph[4][1] = 20+1.25;
-    graph[5][1] = 4+1.25;
+    graph[0][2] = 16 - 1.25;
+    graph[0][3] = 13 - 1.25;
+    graph[4][1] = 20 + 1.25;
+    graph[5][1] = 4  + 1.25;
 
-    for (int i = 0; i < V; i++){
-      for (int j = 0; j < V; j++){
-        printf("%5.2f ", graph[i][j]);
-      }
-      printf("\n");
-    }
-    printf("\n");
+    // for (int i = 0; i < V; i++) {
+    //   for (int j = 0; j < V; j++) {
+    //     printf("%5.2f ", graph[i][j]);
+    //   }
+    //   printf("\n");
+    // }
+    // printf("\n");
 
     /* graph I used in this toy test, source node is 0, and sink node is 1
     double graph[V][V] = {{0, 0,  16, 13, 0,  0},
